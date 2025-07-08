88832902a0097bde30639e5010e6fca57efe0792
Pranta Saha <prantasaha907@gmail.com>
2025-07-08 13:11:05 +0600

    Spring Set up

---
+++ StudyScheduler/src/main/java/com/example/StudyScheduler/StudySchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudyScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySchedulerApplication.class, args);
+	}
+
+}
---
+++ StudyScheduler/src/test/java/com/example/StudyScheduler/StudySchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudyScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

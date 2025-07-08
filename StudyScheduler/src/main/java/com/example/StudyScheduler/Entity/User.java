94b1c23f0e8ecc1ce601bb8fba3219ec209833fc
Pranta Saha <prantasaha907@gmail.com>
2025-07-08 13:43:09 +0600

    User entity_1

--- StudyScheduler/src/main/java/com/example/StudyScheduler/Entity/User.java
+++ StudyScheduler/src/main/java/com/example/StudyScheduler/Entity/User.java
@@ -1,5 +1,6 @@
 package com.example.StudyScheduler.Entity;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -7,7 +8,16 @@ import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,17 +29,37 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User implements UserDetails{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true,nullable = false)
     private String email;
+
+    @Column(unique = true,nullable = false)
     private String password;
+
+    @Column(unique = true,nullable = false)
     private String firstName;
+    
+    @Column(unique = true,nullable = false)
     private String lastname;
 
+    @Enumerated(EnumType.STRING)
     private StudyLevel studyLevel;
 
+    @Column(name = "create_at")
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<StudyPlan> studyPlans;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<StudySession> studySessions;
+
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {

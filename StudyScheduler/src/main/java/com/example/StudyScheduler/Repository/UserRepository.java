e93ede2637257a41ba8a900fa28ed0963d147dff
Pranta Saha <prantasaha907@gmail.com>
2025-07-10 12:03:29 +0600

    User Repository

---
+++ StudyScheduler/src/main/java/com/example/StudyScheduler/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.StudyScheduler.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.StudyScheduler.Entity.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long>{
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
